cbae7753b17f527b6495e262504888d0bcb3ce38
Michael Zheng <tradrejoe@gmail.com>
2017-07-22 10:33:16 -0700

    adding jgraphx to install.sh

--- lib/install.sh
+++ lib/install.sh
@@ -5,3 +5,5 @@ mvn install:install-file -Dfile="bcpg-jdk15-133.jar" -DgroupId="org.bouncycastle
 mvn install:install-file -Dfile="bcprov-jdk15-133.jar" -DgroupId="org.bouncycastle" -DartifactId=bcprov -Dversion="1.3.3" -Dpackaging=jar
 
 mvn install:install-file -Dfile="ojdbc14-10.2.0.2.jar" -DgroupId="oracle" -DartifactId=ojdbc14 -Dversion="10.2.0.2" -Dpackaging=jar
+
+mvn install:install-file -Dfile="jgraphx.jar" -DgroupId="jgraphx" -DartifactId="jgraphx" -Dversion="1.0.0" -Dpackaging=jar
